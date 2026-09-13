879a733d5c680edcb9151df39295e8651bb51c79
Brian Mwendia <brayomwendia6@gmail.com>
2026-09-13 23:53:24 +0300

    My first commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from utils import square, is_even, celsius_to_fahrenheit
+
+number =float(input("Enter a number: "))
+
+print(f"The square: {square(number)}")
+print(f"Even: {is_even(number)}")
+print(f"Celsius: {celsius_to_fahrenheit(number)}")
+
+      
+      
---
+++ src/utils.py
@@ -0,0 +1,7 @@
+def square(x):
+    return x **2
+
+def is_even(x):
+    return x % 2 == 0
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
